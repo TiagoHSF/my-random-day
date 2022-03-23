5f97960ede69a46d57ca249497ec78e219a89ffa
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-03-22 22:19:40 -0300

    icon loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-day-randomized';
 
+  carregando: any;
   itens: string[] = [];
   randomizedNumber: any;
   itemSelecionado: any;
   substringRedirect: any;
 
-constructor() {
-  this.randomizar()
-}
+  constructor() {
+    this.randomizar();
+  }
 
-ngOnInit(): void {
-  this.itemSelecionado = '...'
-}
+  ngOnInit(): void {
+    this.itemSelecionado = '...';
+    this.carregando = true;
+  }
 
-  randomizar(){
-    this.itens = ['STUDY 30 MINUTES',
+  randomizar() {
+    this.itens = [
+      'STUDY 30 MINUTES',
       'STUDY 1 HOUR',
       'STUDY 2 HOURS',
       'READ A BOOK OF TERROR',
@@ -31,17 +34,17 @@ ngOnInit(): void {
       'FOR A WALK',
       'FOR A SWIM',
       'TO THE GYM',
-      'TO RUN']
-  this.randomizedNumber = (Math.floor(Math.random() * 10))
-  this.itemSelecionado = this.itens[this.randomizedNumber]
+      'TO RUN',
+    ];
+    this.randomizedNumber = Math.floor(Math.random() * 10);
+    this.itemSelecionado = this.itens[this.randomizedNumber];
+    this.carregando = false;
   }
 
-  redirectTo(){
-    this.substringRedirect = this.itemSelecionado.substring(0, 5)
-    if(this.substringRedirect === 'STUDY'){
-      window.open("encurtador.com.br/dxHP1")
+  redirectTo() {
+    this.substringRedirect = this.itemSelecionado.substring(0, 5);
+    if (this.substringRedirect === 'STUDY') {
+      window.open('www.encurtador.com.br/dxHP1');
     }
   }
 }
-
-
